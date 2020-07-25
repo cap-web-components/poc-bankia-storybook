511347e9b6aee47b1b7405c3e824ab6a08aa02d0
mmirca <marius.mirca@gmail.com>
2020-07-25 21:09:40 +0200

    chore(demo): build initial demo

---
+++ docs/app-root.js
@@ -0,0 +1,25 @@
+import { LitElement, html } from 'lit-element';
+import { defineCustomElement } from '~core/utils';
+import Variables from '~shared/variables';
+import Styles from '~shared/styles';
+import AppRootStyles from './app-root.styles';
+
+export class AppRoot extends LitElement {
+
+  static get styles() {
+    return [
+      Variables,
+      Styles,
+      AppRootStyles
+    ];
+  }
+
+  render() {
+    return html`
+      <h1>App root</h1>
+    `;
+  }
+
+}
+
+defineCustomElement('app-root', AppRoot);
---
+++ docs/components/app-events/app-button.stories.js
@@ -0,0 +1,7 @@
+import './app-button';
+
+export default {
+  title: 'app-button'
+};
+
+export const primary = () => `<app-button>Primary Button</app-button>`;
